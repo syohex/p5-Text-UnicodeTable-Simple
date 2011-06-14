4baf6d2764438078cebf6c0d127334605ff56bde
Syohei YOSHIDA <syohex@gmail.com>
2011-06-14 10:29:51 +0900

    init repos

---
+++ lib/Text/UnicodeTable/Simple.pm
@@ -0,0 +1,41 @@
+package Text::UnicodeTable::Simple;
+
+use strict;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Text::UnicodeTable::Simple -
+
+=head1 SYNOPSIS
+
+  use Text::UnicodeTable::Simple;
+
+=head1 DESCRIPTION
+
+Text::UnicodeTable::Simple is
+
+=head1 AUTHOR
+
+Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011- Syohei YOSHIDA
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
