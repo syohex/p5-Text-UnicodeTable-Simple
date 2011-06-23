22642d420cb5d6663e8cd328284773229d29ea1a
Syohei YOSHIDA <syohex@gmail.com>
2011-06-23 22:03:45 +0900

    Add sample program which using japanese full widht font

---
+++ eg/japanese_sample.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use lib qw(../lib);
+
+use utf8;
+use YAML;
+use Text::UnicodeTable::Simple;
+
+binmode STDOUT, ":utf8";
+
+my $t = Text::UnicodeTable::Simple->new;
+
+$t->setCols('Id','Name','Price');
+$t->addRow(1,'りんご',24.4);
+#die YAML::Dump($t);
+$t->addRow(2,'パイナップル',21.2);
+$t->addRowLine();
+$t->addRow(3,'パッションフルーツ',12.3);
+$t->addRowLine();
+$t->addRow('','Total',57.9);
+print $t->draw;
