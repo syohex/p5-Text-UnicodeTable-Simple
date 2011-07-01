5e465bbb6617cc3c46823a0ec28749a7e4292aa7
Syohei YOSHIDA <syohex@gmail.com>
2011-07-01 22:31:46 +0900

    fix section order.

--- lib/Text/UnicodeTable/Simple.pm
+++ lib/Text/UnicodeTable/Simple.pm
@@ -403,11 +403,6 @@ Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
 
 Copyright 2011- Syohei YOSHIDA
 
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
 =head1 SEE ALSO
 
 L<Text::ASCIITable>
@@ -415,5 +410,10 @@ L<Text::ASCIITable>
 L<Text::SimpleTable>
 
 L<Text::Table>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
