fcb882cbecb4c2ed87d88a89e81073bccc8e29bc
Syohei YOSHIDA <syohex@gmail.com>
2011-11-17 22:49:01 +0900

    update japanese sample

--- eg/japanese_sample.pl
+++ eg/japanese_sample.pl
@@ -11,6 +11,7 @@ binmode STDOUT, ":utf8";
 
 my $t = Text::UnicodeTable::Simple->new;
 
+# using alias method of Text::ASCIITable
 $t->setCols('Id','Name','Price');
 $t->addRow(1,'りんご',24.4);
 $t->addRow(2,'パイナップル',21.2);
@@ -20,11 +21,15 @@ $t->addRowLine();
 $t->addRow('','Total',57.9);
 print $t->draw;
 
+print "\n";
+
 my $no_border_t = Text::UnicodeTable::Simple->new(border => 0);
 
-$no_border_t->setCols('Id','Name','Price');
-$no_border_t->addRow(1,'りんご',24.4);
-$no_border_t->addRow(2,'パイナップル',21.2);
-$no_border_t->addRow(3,'パッションフルーツ',12.3);
-$no_border_t->addRow('','Total',57.9);
+$no_border_t->set_header('Id','Name','Price');
+$no_border_t->add_rows(
+    [1,  'りんご', 24.4],
+    [2,  'パイナップル', 21.2],
+    [3,  'パッションフルーツ', 12.3],
+    ['', 'Total', 57.9],
+);
 print $no_border_t->draw;
