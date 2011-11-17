c89271501bfd12f93f2b54ae1561910f066f775b
Syohei YOSHIDA <syohex@gmail.com>
2011-11-17 22:00:51 +0900

    add no border sample

--- eg/japanese_sample.pl
+++ eg/japanese_sample.pl
@@ -19,3 +19,12 @@ $t->addRow(3,'パッションフルーツ',12.3);
 $t->addRowLine();
 $t->addRow('','Total',57.9);
 print $t->draw;
+
+my $no_border_t = Text::UnicodeTable::Simple->new(border => 0);
+
+$no_border_t->setCols('Id','Name','Price');
+$no_border_t->addRow(1,'りんご',24.4);
+$no_border_t->addRow(2,'パイナップル',21.2);
+$no_border_t->addRow(3,'パッションフルーツ',12.3);
+$no_border_t->addRow('','Total',57.9);
+print $no_border_t->draw;
